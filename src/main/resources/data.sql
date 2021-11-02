33f94a1d7bd389d6afc38523cd1f3fe58a3cd197
Rodrigo Nunes <rodrigongalvan@hotmail.com>
2021-11-02 10:29:52 -0300

    Habilitando carregamento do DB automaticamente através do arquivo data.sql e schema.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,5 @@
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
-INSERT INTO PRODUCT(name, description, price) VALUES('Celular Sansung S20', 'Celular de última geração com 2 anos de garantia', 3999.00);
+INSERT INTO PRODUCT(id, name, description, price) VALUES(1, 'Celular Sansung S21', 'Celular de última geração com 2 anos de garantia', 8999.00);
+INSERT INTO PRODUCT(id, name, description, price) VALUES(2, 'Notebook Dell Aspire', 'Notebook com alta performance', 5999.00);
+INSERT INTO PRODUCT(id, name, description, price) VALUES(3, 'Celular Xiaomi Redmi Note 9 Pró', 'Celular com melhor custo benefício', 1499.00);
+INSERT INTO PRODUCT(id, name, description, price) VALUES(4, 'Celular Iphone 13 Pró Max', 'Celular mais desejado do mundo chegou em nossa loja', 13999.00);
+INSERT INTO PRODUCT(id, name, description, price) VALUES(5, 'Smart Tv Sansung 4K 85 Qled', 'Desfrute do máximo da qualidade de imagem 4K', 17999.00);
